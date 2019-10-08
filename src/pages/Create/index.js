1dbca46cfe46b803afca2d9b4f194b049c258e93
clebercar <contatoclebercarvalho@gmail.com>
2019-10-08 01:12:51 -0300

    Implementa formulário de criação

--- src/pages/Create/index.js
+++ src/pages/Create/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
+import { 
+  Container,
+  Form,
+  Button
+} from 'react-bootstrap';
 
-// import { Container } from './styles';
+import api from '../../services/api';
+import Header from '../../components/Header';
 
 export default function create() {
+  async function handleSubmit(e){
+    e.preventDefault()
+
+    const data = {}
+    data.title = e.target.title.value
+    data.uri = e.target.uri.value
+    data.tags = e.target.tags.value
+    data.description = e.target.description.value
+
+    const response = await api.post('tools', data);
+
+    if (response.status === 200) {
+      console.log('cadastrado');
+    }
+  }
+
   return (
-    <div>
-        <form>
-            <input type="text" placeholder="Nome da ferramenta" />
-            <input type="url" placeholder="Link da ferramenta"/>
-            <input type="text" placeholder="Tags" />
-            <textarea rows="4" cols="50"></textarea>
-            <button type="submit">Cadastrar</button>
-        </form>
-    </div>
+    <>
+      <Header />
+      <Container>
+        <h1>Cadastrar Ferramenta</h1>
+        <Form onSubmit={(e) => handleSubmit(e)}>
+          <Form.Group >
+            <Form.Control name="title" type="text" placeholder="Nome da ferramenta" />
+          </Form.Group>
+          <Form.Group >
+            <Form.Control name="uri" type="url" placeholder="Link da ferramenta"/>
+          </Form.Group>
+          <Form.Group >
+            <Form.Control name="tags" type="text" placeholder="Tags" />
+          </Form.Group>
+          <Form.Group >
+            <Form.Control name="description" as="textarea" rows="3"></Form.Control>
+          </Form.Group>
+            
+          <Button variant="outline-primary" type="submit">Cadastrar</Button>
+        </Form>
+      </Container>
+    </>
   );
 }
