f50f44e2ea76ae2d836b42650542de9411c7272d
clebercar <contatoclebercarvalho@gmail.com>
2019-10-08 01:13:20 -0300

    Remove Styled components não mais utilizados

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,35 +1,18 @@
 import React from 'react';
-
-import { 
-    Container, 
-    Navbar, 
-    Button, 
-    SearchInput,
-    InputIcon,
-} from './styles';
+import { Container } from 'react-bootstrap';
 
 import Tools from '../../components/Tools';
 import Categories from '../../components/Categories';
-
-import { MdAdd, MdSearch } from "react-icons/md";
+import Header from '../../components/Header';
 
 export default function List() {
   return (
-    <Container>
-        <Navbar>
-            <form>
-                <InputIcon>
-                    <MdSearch/>
-                    <SearchInput type="text" placeholder="Search"/>
-                </InputIcon>
-            </form>
-            <Button type="submit">
-                <MdAdd /> add
-            </Button>
-        </Navbar>
-
-        <Categories />
-        <Tools />
-    </Container>
+    <>
+        <Header />
+        <Container>
+            <Categories />
+            <Tools />
+        </Container>
+    </>
   );
 }
